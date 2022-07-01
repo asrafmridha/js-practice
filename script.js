56e336484eef77c9e050fdac7a4e6b7a5f57a2a2
asraf-mridha <asrafmridha@gmail.com>
2022-07-02 00:04:41 +0600

    js practice

---
+++ script.js
@@ -0,0 +1,144 @@
+/* document.write('hello world '   ); 
+    document.write(11+12);
+
+    console.log('Hello world');
+    console.log(2+4);
+     document.getElementById('h1').innerHTML= "hello js";
+
+    prompt ('2 +2 = ?');
+
+var message = "i learn about js";
+
+ let c= "hlw world";
+
+ console.log(c);
+
+ const x=10;
+
+ const y=5;
+ console.log(x+y);
+
+
+ function myfunction(){
+
+    document.getElementById('demo').innerHTML= 'Changed';
+ }
+
+ const firstName= 'Asraf';
+
+ const lastName= 'Mridha';
+
+ console.log (firstName + ' '+ lastName);
+
+ */
+
+//let function baire use kora jabe na
+/* if (true) {
+  let x = 5;
+
+  console.log(x);
+}
+
+if (true) {
+
+   var z=6;
+  console.log(z);
+}
+
+ console.log(z);
+
+ {
+
+    var x='asraf';
+ }
+  console.log(x); 
+
+ //array declaretion
+
+  const cars=['toyota','bmw', 'bal'];
+
+  cars[0]="change";
+
+  cars.push('new');
+
+  console.log(cars); 
+
+  let x=10;
+  let y=5;
+
+  x /= y;
+
+  console.log(x);
+
+  let x= 'Asraf';
+
+  let y= 'Mridha';
+
+  console.log(x + ' ' + y);
+
+  let text1= "What a nice ";
+
+  let text2= " day it is";
+
+  text1 += text2;
+
+  console.log(text1); 
+
+  let x= 5+5;
+
+   let y= "5" + 5;
+
+   let z= 'HEllo 5';
+  //ki type data seta bujhate 
+  console.log(typeof x);  
+
+  console.log(typeof z);
+
+  console.log(typeof z); 
+  
+
+  //scientific way
+  let x= 123e5;
+
+  console.log(x); 
+
+  //Bollean Statement
+
+  let x=5;
+
+  let y=6;
+
+  let b= (x==y);
+
+  console.log(b);
+  
+//JavaScript Objects
+
+ let x= 5;
+ let y= 6;
+
+ let z=7;
+
+ let myObjective= { x:5 , y:6, z:7 };
+
+  console.log(myObjective);
+
+  console.log(typeof myObjective); 
+  
+  //undefined javaScript
+  car =undefined;
+
+  console.log(car);  */
+
+  //Empty Value
+
+  let truck= "";
+
+  console.log (truck); //Empty String
+
+
+
+
+
+
+  
