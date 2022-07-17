1b54fef186817bc1fb58780d996b31d3734a554a
asraf-mridha <asrafmridha@gmail.com>
2022-07-17 08:46:10 +0600

    Switch Statement

--- script.js
+++ script.js
@@ -564,11 +564,11 @@
           isleapyear(2000); 
 
           let x= "";
-         console.log(Boolean(x)) ; */
+         console.log(Boolean(x)) ; 
 
          //JavaScript Comparisons
 
-         let age="  ";
+         let age=" 17 ";
          
 
          age =Number(age);
@@ -583,6 +583,34 @@
            voteable = (age < 18) ? "Too young" : "Old enough";
          }
 
-         console.log(voteable);  
+         console.log(voteable);    */
+
+         //The JavaScript Switch Statement
+
+         switch (new Date().getDay()) {
+          case 0:
+            day = "Sunday";
+            break;
+          case 1:
+            day = "Monday";
+            break;
+          case 2:
+             day = "Tuesday";
+            break;
+          case 3:
+            day = "Wednesday";
+            break;
+          case 4:
+            day = "Thursday";
+            break;
+          case 5:
+            day = "Friday";
+            break;
+          case 6:
+            day = "Saturday";
+            default:
+              day= " ";
+        }
+        console.log(day);
 
       
