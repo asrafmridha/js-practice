87102b4a7cb7fc619f62c1ccbb31fc4d01201d38
asraf-mridha <asrafmridha@gmail.com>
2023-09-02 23:37:44 +0600

    Simplify arrow function

--- Modern-Javascript/index.js
+++ Modern-Javascript/index.js
@@ -6,7 +6,11 @@
 // console.log(number());
 
 // arrow function
-let number=() => {
-    return 10;
-}
-console.log(number());
+let number = () => {
+  return 10;
+};
+// Simplify arrow function
+let number1 = () => console.log("10");
+
+// console.log(number());
+number1();
