e3d04365f06452a0a2dacecaa77220873baf0e64
asraf mridha <asrafmridha@gmail.com>
2025-01-23 10:51:50 +0600

    Problem Solving

--- New Folder 23-01-2025/object_task.js
+++ New Folder 23-01-2025/object_task.js
@@ -9,3 +9,51 @@ const colors = {
 };
 
 console.log(colors['golden rod']);
+
+// Task-2
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.passenger = '5';
+console.log(car);
+
+// Task-3
+// Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student.physics.marks);
+
+// Task-4
+// Count the number of properties.
+let student1 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const length = Object.keys(student1).length;
+console.log(length);
+
+// Task-5 (Hard)
+// Loop through an object and print the key-value pairs with their types
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for (let key in myObject) {
+    console.log(`key: ${key} | type: ${typeof myObject[key]}`);
+}
