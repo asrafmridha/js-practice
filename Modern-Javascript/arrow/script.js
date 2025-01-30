bdd693040e833fa8f26c7d7e61e3fd42bbfbf57d
asraf mridha <asrafmridha@gmail.com>
2025-01-30 10:25:38 +0600

    intial Commit

---
+++ Modern-Javascript/arrow/script.js
@@ -0,0 +1,14 @@
+const newArray= [10,20,40];
+const result=newArray.map(number=>number*2);
+console.log(result);
+
+const obj={
+    name:"asraf",
+    email: "asrafmridha@gmail.com",
+    phone : 1776716093
+}
+
+for(const dataa in obj){
+    console.log(dataa[obj]);
+}
+// console.log(obj.email);
---
+++ dd.js
@@ -0,0 +1,8 @@
+let text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+// var obj = JSON.parse(text);
+console.log(text.employees);
+// console.log(obj);
